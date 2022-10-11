983482e8db699d775734e899c44e446d7f4ac957
Reyner-Arias <Rey86581@gmail.com>
2022-10-10 19:05:58 -0600

    Add process concept

---
+++ src/tareaasignacionmemoriaso/Proceso.java
@@ -0,0 +1,37 @@
+package tareaasignacionmemoriaso;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Proceso {
+    private static int consecutivo = 0;
+    private final List<Bloque> allocatedBlocks;
+    private final int lifetime;
+    private final int pid;
+
+    public Proceso(int lifetime) {
+        this.allocatedBlocks = new LinkedList<>();
+        this.lifetime = lifetime;
+        this.pid = consecutivo++;
+    }
+
+    public List<Bloque> getAllocatedBlocks() {
+        return allocatedBlocks;
+    }
+
+    public int getLifetime() {
+        return lifetime;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+    
+    public void requestBlock(Bloque block){
+        allocatedBlocks.add( block);
+    }
+    
+    public Bloque freeFirstBlock(){
+        return allocatedBlocks.remove(1);
+    }
+}
