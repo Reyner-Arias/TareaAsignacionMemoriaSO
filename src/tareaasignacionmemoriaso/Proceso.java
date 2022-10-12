70b4f05a62e0a8ea0d26c779dec713d46ed2f8ba
Reyner-Arias <Rey86581@gmail.com>
2022-10-11 22:39:09 -0600

    Add the logic for the four algorithms

--- src/tareaasignacionmemoriaso/Proceso.java
+++ src/tareaasignacionmemoriaso/Proceso.java
@@ -5,18 +5,36 @@ import java.util.List;
 
 public class Proceso {
     private static int consecutivo = 0;
-    private final List<Bloque> allocatedBlocks;
+    private final List<Bloque> allocatedBlocksFirst;
+    private final List<Bloque> allocatedBlocksBest;
+    private final List<Bloque> allocatedBlocksWorst;
+    private final List<Bloque> allocatedBlocksBuddy;
     private final int lifetime;
     private final int pid;
 
     public Proceso(int lifetime) {
-        this.allocatedBlocks = new LinkedList<>();
+        this.allocatedBlocksFirst = new LinkedList<>();
+        this.allocatedBlocksBest = new LinkedList<>();
+        this.allocatedBlocksWorst = new LinkedList<>();
+        this.allocatedBlocksBuddy = new LinkedList<>();
         this.lifetime = lifetime;
         this.pid = consecutivo++;
     }
 
-    public List<Bloque> getAllocatedBlocks() {
-        return allocatedBlocks;
+    public List<Bloque> getAllocatedBlocksFirst() {
+        return allocatedBlocksFirst;
+    }
+    
+    public List<Bloque> getAllocatedBlocksBest() {
+        return allocatedBlocksBest;
+    }
+    
+    public List<Bloque> getAllocatedBlocksWorst() {
+        return allocatedBlocksWorst;
+    }
+    
+    public List<Bloque> getAllocatedBlocksBuddy() {
+        return allocatedBlocksBuddy;
     }
 
     public int getLifetime() {
@@ -27,17 +45,40 @@ public class Proceso {
         return pid;
     }
     
-    public void requestBlock(Bloque block){
-        allocatedBlocks.add( block);
+    public void requestBlockFirst(Bloque block){
+        allocatedBlocksFirst.add( block);
     }
     
-    public Bloque freeFirstBlock(){
-        return allocatedBlocks.remove(1);
+    public void requestBlockBest(Bloque block){
+        allocatedBlocksBest.add( block);
+    }
+    
+    public void requestBlockWorst(Bloque block){
+        allocatedBlocksWorst.add( block);
+    }
+    
+    public void requestBlockBuddy(Bloque block){
+        allocatedBlocksBuddy.add( block);
+    }
+    
+    public Bloque freeBlockFirst(){
+        return allocatedBlocksFirst.remove(1);
+    }
+    
+    public Bloque freeBlockBest(){
+        return allocatedBlocksBest.remove(1);
+    }
+    
+    public Bloque freeBlockWorst(){
+        return allocatedBlocksWorst.remove(1);
+    }
+    
+    public Bloque freeBlockBuddy(){
+        return allocatedBlocksBuddy.remove(1);
     }
     
     @Override
-
     public String toString() {
-        return "Proceso{" + "allocatedBlocks=" + allocatedBlocks.toString() + ", lifetime=" + lifetime + '}';
-    } 
+        return "Proceso{" + "allocatedBlocksFirst=" + allocatedBlocksFirst.toString() + ", allocatedBlocksBest=" + allocatedBlocksBest.toString() + ", allocatedBlocksWorst=" + allocatedBlocksWorst.toString() + ", allocatedBlocksBuddy=" + allocatedBlocksBuddy.toString() + ", lifetime=" + lifetime + '}';
+    }
 }
--- src/tareaasignacionmemoriaso/TareaAsignacionMemoriaSO.java
+++ src/tareaasignacionmemoriaso/TareaAsignacionMemoriaSO.java
@@ -5,7 +5,11 @@ import java.util.List;
 import java.util.Random;
 
 public class TareaAsignacionMemoriaSO {
-    private static List<Bloque> emptyBlocks; // Lista que simula los bloques libres de la memoria
+    // Listas que simulan los bloques libres de las memorias
+    private static List<Bloque> emptyBlocksFirst; 
+    private static List<Bloque> emptyBlocksBest;
+    private static List<Bloque> emptyBlocksWorst;
+    private static List<Bloque> emptyBlocksBuddy;
     
     public static int getIndex(List<Bloque> blocks, Bloque newBlock){
         for (Bloque block : blocks) {
@@ -16,31 +20,30 @@ public class TareaAsignacionMemoriaSO {
         return blocks.size();
     }
     
-    public static void requestMemory(Proceso currentProcess, int memory){
+    public static void requestMemoryFirst(Proceso currentProcess, int memory){
         // Pedir memoria
         boolean emptyBlock = false; // Esta bandera sirve para identificar si el bloque de memoria después de una asignación queda con un valor de 0 en la variable memoria para así eliminarlo de las lista
         int emptyBlockIndex = -1; // Index del bloque con 0 en el atributo memoria, si es que existe
-        for (Bloque block : emptyBlocks) {
+        for (Bloque block : emptyBlocksFirst) {
             if(block.getMemory() >= memory){
                 Bloque dinamicMem = new Bloque(block.getAddress(), memory);
                 block.setAddress(block.getAddress()+memory);
                 block.setMemory(block.getMemory()-memory);
                 if(block.getMemory() == 0){
                    emptyBlock = true;
-                   emptyBlockIndex = emptyBlocks.indexOf(block);
+                   emptyBlockIndex = emptyBlocksFirst.indexOf(block);
                 }
-                currentProcess.requestBlock( dinamicMem);
+                currentProcess.requestBlockFirst( dinamicMem);
                 break;
             }
         }
         if(emptyBlock){
-            emptyBlocks.remove(emptyBlockIndex);
+            emptyBlocksFirst.remove(emptyBlockIndex);
         }
     }
     
-    public static void releaseMemory(Proceso currentProcess){
+    public static void releaseMemory(Bloque freeBlock, List<Bloque> emptyBlocks){
         // Liberar memoria
-        Bloque freeBlock = currentProcess.freeFirstBlock();
         int index = getIndex(emptyBlocks, freeBlock);
         boolean success = false; // Bandera que nos dice si el bloque liberado se combinó con algún otro
         
@@ -80,23 +83,36 @@ public class TareaAsignacionMemoriaSO {
     }
     
     public static void main(String[] args) {
-        emptyBlocks = new LinkedList<>(); 
+        emptyBlocksFirst = new LinkedList<>();
+        emptyBlocksBest = new LinkedList<>();
+        emptyBlocksWorst = new LinkedList<>();
+        emptyBlocksBuddy = new LinkedList<>();
+        
         int totalMemory = 1000000; // Memoria total inicial
         List<Proceso> processes = new LinkedList<>();
         
-        Bloque init = new Bloque(0, totalMemory); // Inicializar el primer bloque libre (la memoria completa)
-        emptyBlocks.add(init); // Añardilo a la lista de bloques vacíos
+        Bloque initFirst = new Bloque(0, totalMemory); // Inicializar el primer bloque libre (la memoria completa) First Fit
+        Bloque initBest = new Bloque(0, totalMemory); // Inicializar el primer bloque libre (la memoria completa) Best Fit
+        Bloque initWorst = new Bloque(0, totalMemory); // Inicializar el primer bloque libre (la memoria completa) Worst Fit
+        Bloque initBuddy = new Bloque(0, totalMemory); // Inicializar el primer bloque libre (la memoria completa) Buddy System
+        
+        emptyBlocksFirst.add(initFirst); // Añardilo a la lista de bloques vacíos First Fit
+        emptyBlocksFirst.add(initBest); // Añardilo a la lista de bloques vacíos Best Fit
+        emptyBlocksFirst.add(initWorst); // Añardilo a la lista de bloques vacíos Worst Fit
+        emptyBlocksFirst.add(initBuddy); // Añardilo a la lista de bloques vacíos Buddy System
         
         Random random = new Random();
         random.setSeed(12345L); // Establecer semilla para los valores randomizados
         
         for(int i = 0; i < 100; i++){ // For para inicializar los 100 procesos
             Proceso process = new Proceso(random.nextInt(271)+30); // Crear una nueva instancia de la clase Proceso con un tiempo de vida de 30 a 300 segundos
-            requestMemory(process, random.nextInt(151)+50); // Solicitar el bloque de memoria inicial de 50 a 200 bytes
+            int ranNum = random.nextInt(151)+50;
+            requestMemoryFirst(process, ranNum); // Solicitar el bloque de memoria inicial de 50 a 200 bytes
+            //requestMemoryBest(process, ranNum);
+            //requestMemoryWorst(process, ranNum);
+            //requestMemoryBuddy(process, ranNum);
             System.out.println(process.toString());
             processes.add(process);
         }
-        
-        
     }
 }
