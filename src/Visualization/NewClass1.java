9de0da223135f60bca376973ba9e73825311d778
Davenb42 <david.benavides.n@gmail.com>
2022-10-16 11:14:57 -0600

    Visualization

---
+++ src/Visualization/NewClass1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Visualization;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Dell
+ */
+public final class NewClass1 {
+    public NewClass1(Graphics g){
+        g.setColor(Color.blue);
+        g.fillRect(10,170, 1000, 50);
+    }
+}
